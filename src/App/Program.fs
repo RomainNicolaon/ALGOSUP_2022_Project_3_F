5276f65eb962e9c5082ced234fcdca00bac1b969
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2022-01-11 10:02:29 +0100

    Creation of the tree structure
    - add App folder
    - add library folder
    - add test folder

---
+++ src/App/Program.fs
@@ -0,0 +1,16 @@
+﻿module main
+
+open System
+open SFML.Audio
+open System.Threading
+
+let PlaySound = 
+    let buffer = new SoundBuffer("test.wav")
+    let sound  = new Sound(buffer)
+    sound.Play()
+
+    while sound.Status = SoundStatus.Playing do 
+        Thread.Sleep(100)
+
+        
+PlaySound
---
+++ src/Library/Library.fs
@@ -0,0 +1,8 @@
+﻿module FsharpAudio
+
+open System
+open System.IO
+open System.Runtime.InteropServices
+open System.Security
+open SFML.System
+
