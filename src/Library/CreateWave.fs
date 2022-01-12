5059ddc23288a4396b5e1234a2ca1d8e66f199e7
elisegtr <elise.gautier@algosup.com>
2022-01-12 13:59:36 +0100

    add code and comments to trace the waveforms

--- src/Library/CreateWave.fs
+++ src/Library/CreateWave.fs
@@ -1,18 +1,24 @@
 module CreateWave
 open System
 
+let sample x = (x + 1.)/2. * 255. |> byte
 let Pi = Math.PI
-let Amplitude = 1.
-let Frequency = 40.
+let Amplitude = 1. // Measurement of its change in a single period
+let Frequency = 40. // Hertz
 let limit = 500
 
+
+// Periodic calculation for sine wave
 let sinus = Array.init limit (fun i -> Amplitude * sin((Frequency/ Pi) * float i)) //Sinusoidale
 
 
-//let sawTooth = Array.init limit (fun i -> (Amplitude * (((2. * float i/ Frequency))  2.) - Amplitude))
+// Periodic calculation for sawtooth wave
+let sawTooth = Array.init limit (fun i -> (2.*Amplitude/Pi)* Math.Atan(tan((2.*Pi*float i)/Frequency))  )
 
 
+// Periodic calculation for triangle wave
 let triangle = Array.init limit (fun i ->  (2.*Amplitude/Pi)* Math.Asin(sin((2.*Pi*float i)/Frequency))  )
 
 
+// Periodic calculation for squrae wave
 let square = Array.init limit (fun i -> float(Math.Sign(sin(2. * Math.PI * float i*(Frequency/float limit)))))
