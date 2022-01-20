01c4273a9554633787f6e7b4c3476044731fec8b
Nicolaon Romain <78691006+RomainNicolaon@users.noreply.github.com>
2022-01-20 18:15:04 +0100

    Update Filter.fs

--- src/Library/Filter.fs
+++ src/Library/Filter.fs
@@ -25,20 +25,3 @@ module Filter =
         let wavesum = // sum of two waves
             startmusic |> List.append(timeDelay2) |> List.append(echowave) |> List.append(EndMusic)|> List.rev
         wavesum
-
-
-
-open System.Threading
-open SFML.Audio
-open SFML.System
-
-
-module Filter =
-
-    let reverb = 
-        // calculation by applying the Sabine Formula
-        let delay = (60.0 / 0.2) * 1000.0
-        let blanche = delay * 2.0
-        let croche = delay / 2.0
-        delay
-
