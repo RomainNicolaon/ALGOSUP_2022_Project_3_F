faa38a57253911fb7aa832ae5defba54f214f9c0
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2022-02-01 16:58:34 +0100

    Update Filter.fs

--- src/Library/Filter.fs
+++ src/Library/Filter.fs
@@ -19,12 +19,13 @@ module Filter =
         let echowave =        
             [for i in  1 .. wave.Length-1  do if i <= timeDelay then yield wave.Item(i) else yield 0.  ] |> List.filter( fun x -> x <> 0.) 
         let echowave =  amplitude echowave amp
+        let echowave2 =  amplitude echowave (amp/2.) 
+
         let EndMusic = 
             [for i in timeDelay .. wave.Length-1 do yield wave.Item(i) ] 
         let timeDelay2 = sinusWave 0.2 0. 0.
         let wavesum = // sum of two waves
-            startmusic |> List.append(timeDelay2) |> List.append(echowave) |> List.append(EndMusic)|> List.rev
-        wavesum
+           startmusic |> List.append(timeDelay2) |> List.append(echowave) |> List.append(timeDelay2)|> List.append(echowave2)|>List.append(timeDelay2) |> List.append(EndMusic)|> List.rev
 
 
 
