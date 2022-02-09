f465f04ede51eb9ec901aa1a86929cf676f270de
romain.nicolaon <nicolaon.romain@gmail.com>
2022-02-09 10:51:50 +0100

    add titanic song
    add titanic song directly in main branch because other branch is out of date
    
    Co-Authored-By: elisegtr <91249847+elisegtr@users.noreply.github.com>

--- src/Library/CreateWave.fs
+++ src/Library/CreateWave.fs
@@ -8,21 +8,68 @@ module CreateWave =
 
     type noteLetter  =
         | A
+        | As
+
         | B
+        | B2
+
         | C
+        | Cs
+        | C2
+        | Cs2
+
         | D
+        | Ds
+        | Ds2
+
         | E
+        | E2
+
         | F
+        | Fs
+        | Fs2
+
         | G
+        | Gs
+        | Gs2
+
     let Note note  = 
-                match note with
-                | A -> 587.
-                | B -> 659.
-                | C -> 426.63
-                | D -> 459.
-                | E -> 495.
-                | F -> 514.
-                | G -> 523.
+            match note with
+            // AuClairDeLaLune notes
+            // | A -> 587.
+            // | B -> 659.
+            // | C -> 426.63
+            // | D -> 459.
+            // | E -> 495.
+            // | F -> 514.
+            // | G -> 523.
+            
+            // Titanic notes
+            | A -> 880. //LA
+            | As -> 932. //LA#
+
+            | B -> 493.//SI
+            | B2 -> 987. //SI2
+
+            | C -> 523. //DO
+            | Cs -> 554. //DO#
+            | C2 -> 1046. //DO2
+            | Cs2 -> 1108. //DO2#
+
+            | D -> 587. //RE
+            | Ds -> 622. //RE#
+            | Ds2 -> 1244. //RE2#
+
+            | E -> 659. //MI
+            | E2 -> 1318. //MI2
+
+            | F -> 698. //FA
+            | Fs -> 739. //FA#
+            | Fs2 -> 1479. //FA2#
+
+            | G -> 783. //SOL
+            | Gs -> 830. //SOL#
+            | Gs2 -> 1661. //SOL2#
 
 
     // Periodic calculation for sine wave
