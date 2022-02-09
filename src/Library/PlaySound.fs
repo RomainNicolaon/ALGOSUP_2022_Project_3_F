c1d2ec8afeea506e00d867f62e558aba3b2856ca
Thomas Planchard <91249646+thomas-planchard@users.noreply.github.com>
2022-02-09 13:47:34 +0100

    Add mp3 conversion, add playsound without save it

--- src/Library/PlaySound.fs
+++ src/Library/PlaySound.fs
@@ -20,3 +20,18 @@ module PlaySound =
 
         do while sound.Status = SoundStatus.Playing do 
             Thread.Sleep(100)
+    
+    let PlaySoundWithoutSaving (way:string)  = 
+        let buffer = new SoundBuffer(way)
+        let sound  = new Sound(buffer)
+        //initialise the time to begin the audio at this time
+        let t = Time.FromMilliseconds(10000) 
+
+        //put the audio at the time select before
+        do sound.set_PlayingOffset(t)
+        //Play the sound at the time select or at the beginning if we don't select time
+        do sound.Play()
+
+        do while sound.Status = SoundStatus.Playing do 
+            Thread.Sleep(100)
+        File.delete(way)
